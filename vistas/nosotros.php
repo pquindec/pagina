06af670546e930f79da23ae1c196af9a8aa2fae1
pquindec <xndre.qc@gmail.com>
2021-11-17 20:39:22 -0500

    modificar titulo

--- vistas/nosotros.php
+++ vistas/nosotros.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Frontend Store</title>
+    <title>Bows&Crafts</title>
     <link rel="stylesheet" href="../css/bootstrap.css" type="text/css">
     <link rel="stylesheet" href="../css/normalize.css" type="text/css">
 </head>
